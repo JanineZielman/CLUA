b0ca03dbbe7755251e41d43b4678a0b084d28cf4
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2023-06-22 13:48:07 +0200

    update

--- script.js
+++ script.js
@@ -13,6 +13,38 @@ async function logJSONData() {
 
 logJSONData()
 
+const moveContainer = document.getElementById("section-3");
+
+
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+moveContainer.addEventListener("mousemove", (evt) => {
+  const links = moveContainer.getElementsByTagName('a');
+  const links2 = document.getElementById('section-2').getElementsByTagName('a');
+  for (let i = 0; i < links2.length; i++) {
+    console.log(links2[i].clientWidth)
+    if (i % 2 == 0) {
+      links2[i].style.marginLeft = evt.screenY * 0.05  + 'px'
+      links2[i].style.marginTop = evt.screenX * 0.05 + 'px'
+    } else {
+      links2[i].style.marginLeft = evt.screenX * -0.05  + 'px'
+      links2[i].style.marginTop = evt.screenY * -0.05   + 'px'
+    }
+  }
+  for (let i = 0; i < links.length; i++) {
+    if (i % 2 == 0) {
+      links[i].style.marginLeft = evt.screenX * -0.05 + 'px'
+      links[i].style.marginTop = evt.screenY * -0.05 + 'px'
+    } else {
+      links[i].style.marginLeft = evt.screenX * 0.05 + 'px'
+      links[i].style.marginTop = evt.screenY * 0.05  + 'px'
+    }
+  }
+});
+
 
 const scrollContainer = document.getElementById("section-8");
 
